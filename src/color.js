4c798d1e942ac8bcace9210e8c65f0a78414a986
Juho Vepsalainen <bebraw@gmail.com>
2012-05-02 12:54:47 +0300

    Add paranoia check

--- src/color.js
+++ src/color.js
@@ -164,9 +164,9 @@
 
         // based on http://www.phpied.com/rgb-color-parser-in-javascript/
         return {
-            r: parseInt(hex.substring(0, 2), 16) / 255,
-            g: parseInt(hex.substring(2, 4), 16) / 255,
-            b: parseInt(hex.substring(4, 6), 16) / 255
+            r: parseInt(hex.substring(0, 2), 16) / 255 || 0,
+            g: parseInt(hex.substring(2, 4), 16) / 255 || 0,
+            b: parseInt(hex.substring(4, 6), 16) / 255 || 0
         };
     };
 
